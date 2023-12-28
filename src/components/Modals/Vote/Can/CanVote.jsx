e8ee87c2c0b78ef8a9238cba92218e8ee43262e4
bigkid <egbeyemifisola777@gmail.com>
2023-12-28 20:56:54 +0100

    newer changes

--- src/components/Modals/Vote/Can/CanVote.jsx
+++ src/components/Modals/Vote/Can/CanVote.jsx
@@ -3,7 +3,12 @@ import Avatar from "react-avatar";
 import "./styles.css";
 import { FaCheckCircle, FaClock } from "react-icons/fa";
 
-const CanVote = () => {
+const CanVote = ({ voteOptions, creator, question, duration }) => {
+
+  console.log("Vote Options:", voteOptions);
+  console.log("Creator:", creator);
+  console.log("Question:", question);
+  console.log("Duration:", duration);
   const [votes, setVotes] = useState([0, 0]);
 
   const handleVote = (index) => {
@@ -37,35 +42,39 @@ const CanVote = () => {
       </div>
 
       <div className="vote">
-        <p className="details">What is your preferred programming language</p>
+        <p className="details">{question && question}</p>
         <div className="poll">
-          <div className="lylac" onClick={() => handleVote(0)}>
-            <p style={{ fontSize: "12px", textAlign: "start" }}>Python</p>
-            <div
-              className={`egg-bg ${getHighestIndex() === 0 ? "highest" : ""}`}
-            >
-              {getHighestIndex() === 0 && (
-                <FaCheckCircle className="verified-icon" />
-              )}
-              <div
-                className="egg-bg-before"
-                style={{ width: `${getPercentage(0)}%` }}
-              ></div>
-              <p
-                style={{
-                  color: "white",
-                  position: "absolute",
-                  top: "50%",
-                  right: "5px",
-                  transform: "translateY(-50%)",
-                  textAlign: "center",
-                }}
-              >
-                {getPercentage(0).toFixed(2)}%
-              </p>
-            </div>
-          </div>
-          <div className="lylac" onClick={() => handleVote(1)}>
+          {voteOptions && voteOptions.length > 0 && voteOptions.map((item, index) => (
+             <div className="lylac" onClick={() => handleVote(index)}>
+             <p style={{ fontSize: "12px", textAlign: "start" }}>{item.content}</p>
+             <div
+               className={`egg-bg ${getHighestIndex() === index ? "highest" : ""}`}
+             >
+               {getHighestIndex() === index && (
+                 <FaCheckCircle className="verified-icon" />
+               )}
+               <div
+                 className="egg-bg-before"
+                 style={{ width: `${getPercentage(index)}%` }}
+               ></div>
+               <p
+                 style={{
+                   color: "white",
+                   position: "absolute",
+                   top: "50%",
+                   right: "5px",
+                   transform: "translateY(-50%)",
+                   textAlign: "center",
+                 }}
+               >
+                 {getPercentage(index).toFixed(2)}%
+               </p>
+             </div>
+           </div>
+
+          ))}
+         
+          {/* <div className="lylac" onClick={() => handleVote(1)}>
             <p style={{ fontSize: "12px", textAlign: "start" }}>JavaScript</p>
             <div
               className={`egg-bg egg-bge ${
@@ -92,7 +101,7 @@ const CanVote = () => {
                 {getPercentage(1).toFixed(2)}%
               </p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
 
--- src/pages/Polls/Voting.jsx
+++ src/pages/Polls/Voting.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import MainLayout from "../../Layout/MainLayout";
 import PollsSearch from "../../components/PollsComp/PollsSearch";
@@ -12,6 +11,58 @@ import { Modal } from "react-bootstrap";
 
 const Voting = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [responseData, setResponseData] = useState(null);
+  const [promoted, setPromoted] = useState(null);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const token = localStorage.getItem("authTOken");
+    console.log(`Token ${token}`);
+    const makeRequest = async () => {
+      try {
+        const promotedResponse = await fetch(
+          `http://127.0.0.1:8000/poll/promoted/`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: `Token ${token}`,
+            },
+          }
+        );
+
+        const response = await fetch(`http://127.0.0.1:8000/poll/polls/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Token ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          console.log("Response not ok");
+        }
+
+        const responseBody = await response.json();
+        console.log(responseBody);
+        setResponseData(responseBody);
+
+        const promotedResponseBody = await promotedResponse.json();
+        setPromoted(promotedResponseBody);
+        // Check if responseData is not null before mapping
+      } catch (error) {
+        console.log(error);
+        // Handle errors as needed
+      } finally {
+        // setIsLoading(true); // Move this line if needed based on your requirement
+        console.log("Finally block executed");
+      }
+    };
+
+    makeRequest();
+  }, []);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -41,8 +92,17 @@ const Voting = () => {
             <div className="pollsBox">
               <h2 className="head-line bus-dir">Promoted polls</h2>
               <div className="grid">
-                <CantVote onClick={openModal} />
-                <CantVote />
+                {promoted &&
+                  promoted.length > 1 &&
+                  promoted.map((item, index) => (
+                    <CanVote
+                      key={index}
+                      voteOptions={item.options_list ? item.options_list : []}
+                      creator={item.user ? item.user : "unknown user"}
+                      question={item.question ? item.question : ""}
+                      duration={item.duration ? item.duration : ""}
+                    />
+                  ))}
               </div>
             </div>
 
@@ -53,14 +113,22 @@ const Voting = () => {
             </div>
 
             <div className="col">
-              <CanVote />
-              <CanVote />
-              <Poll />
-              <CanVote />
-              <img src="images/jumia.png" alt="" className="ads-img" />
+              {responseData &&
+                responseData.length > 1 &&
+                responseData.map((item, index) => (
+                  <CanVote
+                    key={index}
+                    voteOptions={item.options_list ? item.options_list : []}
+                    creator={item.user ? item.user : "unknown user"}
+                    question={item.question ? item.question : ""}
+                    duration={item.duration ? item.duration : ""}
+                  />
+                ))}
+
+              {/* <img src="images/jumia.png" alt="" className="ads-img" />
               <CanVote />
               <CanVote onClick={openModal} />
-              <CanVote />
+              <CanVote /> */}
             </div>
           </div>
 
@@ -87,6 +155,54 @@ const Voting = () => {
         </div>
       </MainLayout>
     </div>
+  );
+};
+
+const Votingg = () => {
+  const [responseData, setResponseData] = useState(null);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const token = localStorage.getItem("authTOken");
+    console.log(`Token ${token}`);
+    const makeRequest = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/poll/polls/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Token ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          console.log("Response not ok");
+        }
+
+        const responseBody = await response.json();
+        console.log(responseBody);
+        setResponseData(responseBody);
+
+        // Check if responseData is not null before mapping
+      } catch (error) {
+        console.log(error);
+        // Handle errors as needed
+      } finally {
+        // setIsLoading(true); // Move this line if needed based on your requirement
+        console.log("Finally block executed");
+      }
+    };
+
+    makeRequest();
+  }, []);
+
+  return (
+    <>
+      {responseData &&
+        responseData.length > 1 &&
+        responseData.map((item, index) => <p>Voting</p>)}
+    </>
   );
 };
 
